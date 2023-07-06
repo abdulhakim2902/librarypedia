c3b8897b4dd010d923075ceeefd97421d018ce23
Abdul Hakim <hakim@undercurrent.tech>
2023-07-07 06:14:33 +0700

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = isNaN(parseInt(process.env.PORT)) ? 3000 : +process.env.PORT;
+
+  await initSwagger(app);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
@@ -12,6 +21,31 @@ async function bootstrap() {
   await app.listen(port, () => {
     console.log(`Server is listening on localhost:${port}`);
   });
+}
+
+async function initSwagger(app: INestApplication) {
+  const authConfig = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Auth API description')
+    .build();
+
+  const authDocument = SwaggerModule.createDocument(app, authConfig, {
+    include: [AuthModule],
+  });
+
+  SwaggerModule.setup('explorer/auth', app, authDocument);
+
+  const userConfig = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('User API description')
+    .addBearerAuth()
+    .build();
+
+  const userDocument = SwaggerModule.createDocument(app, userConfig, {
+    include: [UserModule],
+  });
+
+  SwaggerModule.setup('explorer/user', app, userDocument);
 }
 
 bootstrap();
